9726182cc07853839f27496074d1126a4e87e729
John Kirkpatrick <johnkirkpatrickmusic@gmail.com>
2018-07-16 17:36:20 -0700

    Setting up initial Arduino nano program

---
+++ MamaMiCryNano/MamaMiCryNano.ino
@@ -0,0 +1,33 @@
+//Code developed by NV Jumpstarter
+//Last edited by: John Kirkpatrick
+
+
+//D13                         //D12
+//3V3                         //D11 (PWM)
+//AREF                        //D10 (PWM)
+//A0                          //D9  (PWM)
+//A1                          //D8
+//A2                          //D7
+//A3                          //D6  (PWM)
+//A4                          //D5  (PWM)
+//A5                          //D4
+//A6                          //D3  (PWM) - Motor Pin
+//A7                          //D2
+//5V                          //GND
+//RST                         //RST
+//GND                         //RX D0
+//VIN                         //TX D1
+
+
+#define MOTOR_PIN 3
+
+void setup() 
+{
+  pinMode(MOTOR_PIN, OUTPUT);
+
+}
+
+void loop() 
+{
+  
+}
