574dc8f83fb5de638a9e41f84a6ab0fad7fe38e8
John Kirkpatrick <johnkirkpatrickmusic@gmail.com>
2018-07-16 18:33:16 -0700

    Basic layout of program

--- MamaMiCryNano/MamaMiCryNano.ino
+++ MamaMiCryNano/MamaMiCryNano.ino
@@ -12,7 +12,7 @@
 //A4                          //D5  (PWM)
 //A5                          //D4
 //A6                          //D3  (PWM) - Motor Pin
-//A7                          //D2
+//A7                          //D2  Button Pin
 //5V                          //GND
 //RST                         //RST
 //GND                         //RX D0
@@ -20,14 +20,38 @@
 
 
 #define MOTOR_PIN 3
+#define MOTOR_BUTTON 2
+
+byte motorMode = 0;
+
+void changeMotorMode() 
+{
+  motorMode++;
+}
 
 void setup() 
 {
   pinMode(MOTOR_PIN, OUTPUT);
-
+  pinMode(MOTOR_BUTTON, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(MOTOR_BUTTON), changeMotorMode, CHANGE);
 }
 
 void loop() 
 {
-  
+  if(motorMode > 2)
+  {
+    motorMode = 0;
+  }
+  switch(motorMode)
+  {
+    case 0:
+      analogWrite(MOTOR_PIN, 0);
+      break;
+    case 1:
+      analogWrite(MOTOR_PIN, 500);
+      break;
+    case 2:
+      analogWrite(MOTOR_PIN, 1023);
+      break;
+  }
 }
