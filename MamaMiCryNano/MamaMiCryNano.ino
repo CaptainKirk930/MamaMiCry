9c788f49255fe66f4bebe16acbed0e677c2c6394
John Kirkpatrick <johnkirkpatrickmusic@gmail.com>
2018-07-16 20:02:30 -0700

    First test program with deep sleep

--- MamaMiCryNano/MamaMiCryNano.ino
+++ MamaMiCryNano/MamaMiCryNano.ino
@@ -1,6 +1,7 @@
 //Code developed by NV Jumpstarter
 //Last edited by: John Kirkpatrick
 
+#include "LowPower.h"
 
 //D13                         //D12
 //3V3                         //D11 (PWM)
@@ -23,6 +24,9 @@
 #define MOTOR_BUTTON 2
 
 byte motorMode = 0;
+unsigned long timeSinceLastButtonPush = 0;
+unsigned long currentTime = 0;
+unsigned long sleepTime = 0;
 
 void changeMotorMode() 
 {
@@ -38,20 +42,41 @@ void setup()
 
 void loop() 
 {
-  if(motorMode > 2)
+  currentTime = millis();
+  if(motorMode > 3)
   {
     motorMode = 0;
   }
   switch(motorMode)
   {
-    case 0:
+    case 0:  // Motor Off
       analogWrite(MOTOR_PIN, 0);
       break;
-    case 1:
-      analogWrite(MOTOR_PIN, 500);
-      break;
-    case 2:
+    case 1:  // 150 Hz
       analogWrite(MOTOR_PIN, 1023);
+      timeSinceLastButtonPush = millis();
       break;
+    case 2:  // 100 Hz
+      analogWrite(MOTOR_PIN, 512);
+      timeSinceLastButtonPush = millis();
+      break;
+    case 3:  // Heart Beat
+      analogWrite(MOTOR_PIN, 50);
+      timeSinceLastButtonPush = millis();
+      break;
+  }
+  while(digitalRead(MOTOR_BUTTON) == LOW)
+  {
+    if(sleepTime - currentTime > 2000)
+    {
+      timeSinceLastButtonPush = 999999; //Trip the if statement to go into deep sleep
+    }
+    sleepTime = millis();
+  }
+  if(timeSinceLastButtonPush - currentTime > 900000)
+  {
+    analogWrite(MOTOR_PIN, 0);
+    LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
+    detachInterrupt(digitalPinToInterrupt(MOTOR_BUTTON)); 
   }
 }
